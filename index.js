1be08b39601d76ecb24d8de25739aa2543341baf
Patrick Simmons <hadroncollides@gmail.com>
2021-03-17 11:36:35 -0400

    commit tree a6c7cbf510101341e41b063fd8f39bffa308d24e

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue){
   // Write your code here!
+  if(someValue <= 400){
+    return 'This one is on me!'
+  } else if (1999 < someValue && someValue < 2500){
+      return "I will gladly take your thirty bucks."
+  } else {
+    return "No can do."
+  }
 }
 
 function ternaryCheckCity(){
